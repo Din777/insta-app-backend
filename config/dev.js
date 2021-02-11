6a4d80ce0ab2199fbcf6d0028caf64f8cf16b40b
unknown <din4eg@gmail.com>
2021-02-12 00:08:00 +0200

    mongo connection settings changes

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // 'dbURL': 'mongodb://localhost:27017',
-  'dbURL': "mongodb+srv://DinaK:d123456m@cluster0.nfbr9.mongodb.net/?retryWrites=true&w=majority",
+  'dbURL': 'mongodb://localhost:27017',
+  // 'dbURL': "mongodb+srv://DinaK:d123@cluster0.nfbr9.mongodb.net/?retryWrites=true&w=majority",
 
 }
--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  'dbURL': "mongodb+srv://DinaK:d123456m@cluster0.nfbr9.mongodb.net/?retryWrites=true&w=majority",
+  'dbURL': "mongodb+srv://DinaK:d123@cluster0.nfbr9.mongodb.net/?retryWrites=true&w=majority",
 }
  
